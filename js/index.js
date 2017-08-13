345146fa946bae87fc0bce5053cdcd6912726d0c
lordbyron90 <nlagoglez@gmail.com>
2017-08-13 19:33:01 +0200

    Correcciones

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+//MASONRY
 // modified Isotope methods for gutters in masonry
 $.Isotope.prototype._getMasonryGutterColumns = function() {
 	var gutter = this.options.masonry && this.options.masonry.gutterWidth || 0;
@@ -49,51 +50,56 @@ $(document).ready(function(){
     }
   	});
 
-  	//overlayed menu
-      $('.burger_menu').on('click', function() {
-      	var _this = $(this);
-          $('.rainbow_container').toggleClass('animateOverlay');
-          $('.main_header').css('height','auto');
-          _this.toggleClass('open');
-      })
 
 
-    //follow_scroll
-
-    if ( $(".column_text").length ){
-
-		var element = $(".column_text"),
-			originalY = element.offset().top;
-
-		var navHeight = $(".main_header").outerHeight();
-
-		var topMargin = 20,
-			topDistance = '';
-
-        console.log("originalY:", originalY);
-
-
-		$(window).on('scroll', function(event) {
-	    	var scrollTop = $(window).scrollTop();	
-			var topDistance = 0;
-
-			if (scrollTop < originalY){
-				console.log("topDistance 1: ",topDistance);
-				topDistance = 0;
-			}
-			else{
-				topDistance = scrollTop - originalY + topMargin*2 + navHeight;
-			}
-	    
-		    element.stop(false, false).animate({
-	        	top:topDistance
-		    }, 350, 'swing');
+//OVERLAYED MENU
+	//overlayed menu
+  $('.burger_menu').on('click', function() {
+  	var _this = $(this);
+      $('.rainbow_container').toggleClass('animateOverlay');
+      $('.main_header').css('height','auto');
+      _this.toggleClass('open');
+  })
 
 
 
-		});  
 
-    }
+//FOLLOW SCROLL
+
+if ( $(".column_text").length ){
+
+	var element = $(".column_text"),
+		originalY = element.offset().top;
+
+	var navHeight = $(".main_header").outerHeight();
+
+	var topMargin = 20,
+		topDistance = '';
+
+    console.log("originalY:", originalY);
+
+
+	$(window).on('scroll', function(event) {
+    	var scrollTop = $(window).scrollTop();	
+		var topDistance = 0;
+
+		if (scrollTop < originalY){
+			console.log("topDistance 1: ",topDistance);
+			topDistance = 0;
+		}
+		else{
+			topDistance = scrollTop - originalY + topMargin*2 + navHeight;
+		}
+    
+	    element.stop(false, false).animate({
+        	top:topDistance
+	    }, 350, 'swing');
+
+
+
+	});  
+
+}
 
     
 });
